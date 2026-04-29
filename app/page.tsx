98c6d1cf3cffae962cb3398500070ca0e2f3b1ec
Olowokere_Destiny <destinyolowokere@gmail.com>
2026-04-29 09:50:14 +0100

    cert section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CertificationsSection from "@/components/Certification";
 import GuidanceSection from "@/components/GuidanceSection";
 import Header from "@/components/Header";
 import HeroSection from "@/components/Hero";
@@ -10,6 +11,7 @@ export default function Home() {
     <HeroSection />
     <NewsletterSection />
     <GuidanceSection />
+    <CertificationsSection />
     </>
   );
 }
